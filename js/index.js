60e58d8fbdd038b1fbae400d21da7890b1924bdf
jjjjinwoo <somes228@mgail.com>
2024-08-27 22:29:14 +0900

    중간저장

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// 스크롤 트리거 불러오기(플러그인 안정화)
+
+gsap.registerPlugin(ScrollTrigger);
+
 // 마우스 커서 - JS
 
 let mouseCursor = document.querySelector("#cursor");
@@ -7,7 +11,7 @@ window.addEventListener("scroll", cursor);
 window.addEventListener("mousemove", cursor);
 function cursor(e) {
   mouseCursor.style.left = e.pageX + "px";
-  mouseCursor.style.top = e.pageY - scrollY + "px";
+  mouseCursor.style.top = e.pageY + "px";
 }
 
 const mainTextBox = document.querySelector(".main_section .text_box");
@@ -34,4 +38,31 @@ $(function () {
     $(".posX").text(e.pageX);
     $(".posY").text(e.pageY);
   });
+});
+
+// 헤더 색상 반전
+
+let vwHeight = window.innerHeight;
+
+document.addEventListener("scroll", invertHeader);
+
+function invertHeader() {
+  if (scrollY > vwHeight) {
+    header.style.filter = "invert(100%)";
+  } else if (scrollY == 0) {
+    header.style.filter = "invert(0%)";
+  }
+}
+
+// 섹션1 스크롤 트리거 - GSAP
+
+gsap.to(".section1 .img_box", {
+  scrollTrigger: {
+    trigger: ".section1",
+    start: "0% 50%",
+    end: "30% 50%",
+    scrub: 1,
+    markers: false,
+  },
+  transform: "rotateY(0deg)",
 });
